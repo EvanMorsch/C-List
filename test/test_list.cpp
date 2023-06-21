52233e7c92da0f87a6c4a7b73ff73d80c182af6d
Evan Morschhauser <evanmorsch@gmail.com>
2023-06-21 00:13:44 -0400

    Add list_remove_at tests

--- test/test_list.cpp
+++ test/test_list.cpp
@@ -97,6 +97,36 @@ int test_val3 = 63;
         EXPECT_EQ(List_Push(&test_val1, test_list), LIST_ERROR_EXCEED_LIMIT); //LIST_ERROR_EXCEED_LIMIT
     }
 //}
+//List_Remove_At
+//{
+    //Tests a valid usage
+    TEST(ListRemoveAtTest, ValidArgs) {
+        List_t* test_list = List_Create(10, test_cmp_fnc, test_free_fnc);
+
+        EXPECT_EQ(List_Push(&test_val1, test_list), LIST_ERROR_SUCCESS);//add entry
+        EXPECT_EQ(List_Push(&test_val2, test_list), LIST_ERROR_SUCCESS);//add entry
+        EXPECT_EQ(List_Push(&test_val3, test_list), LIST_ERROR_SUCCESS);//add entry
+        EXPECT_EQ(List_Length(test_list), 3);
+
+        EXPECT_EQ(List_Remove_At(1, test_list), &test_val2);//mid entry
+        EXPECT_EQ(List_Length(test_list), 2);
+        EXPECT_EQ(List_Remove_At(1, test_list), &test_val3);//last entry
+        EXPECT_EQ(List_Length(test_list), 1);
+        EXPECT_EQ(List_Remove_At(0, test_list), &test_val1);//first/only entry
+        EXPECT_EQ(List_Length(test_list), 0);
+    }
+    //Test List length with improper args
+    TEST(ListRemoveAtTest, InvalidArgs) {
+        List_t* test_list = List_Create(10, test_cmp_fnc, test_free_fnc);
+
+        EXPECT_EQ(List_Push(&test_val1, test_list), LIST_ERROR_SUCCESS);//add entry
+        EXPECT_EQ(List_Length(test_list), 1);
+
+        EXPECT_EQ(List_Remove_At(1, test_list), nullptr);//oob entry
+        EXPECT_EQ(List_Length(test_list), 1);
+        EXPECT_EQ(List_Remove_At(0, NULL), nullptr);//bad args
+    }
+//}
 //List_Pop/List_Shift
 //{
     //Tests a valid usage
